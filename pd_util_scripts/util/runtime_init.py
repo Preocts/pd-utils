388493a4ae994dd77befb51451d057d66b028d00
Preocts <preocts@gmail.com>
2022-07-29 22:50:08 -0400

    runtime init class

---
+++ pd_util_scripts/util/runtime_init.py
@@ -0,0 +1,90 @@
+"""Setup runtime environment for all scripts."""
+from __future__ import annotations
+
+import argparse
+import logging
+from typing import Sequence
+
+from secretbox import SecretBox
+from secretbox.envfile_loader import EnvFileLoader
+
+
+class RuntimeInit:
+    def __init__(self) -> None:
+        """Setup runtime environment for all scripts."""
+        self.secrets = SecretBox()
+        self.parser = argparse.ArgumentParser(
+            prog="aim-scripts",
+            description="Pagerduty command line utilities.",
+            epilog="See: https://github.com/Preocts/pagerduty-utils",
+        )
+
+    def init_secrets(self, env_file: str | None = None) -> None:
+        """
+        Initialize secrets from a file and environment.
+
+        Args:
+            env_file: The path to the file containing the secrets.
+        """
+        self.secrets.use_loaders(EnvFileLoader(env_file))
+
+    def init_logging(self) -> None:
+        """Init logging level and format. (default: $LOGGING_LEVEL | INFO)"""
+        level = self.secrets.get("LOGGING_LEVEL", "INFO")
+        logging.getLogger().setLevel(level)
+        logging.basicConfig(
+            level=level,
+            format="%(asctime)s - %(levelname)s - %(module)s - %(message)s",
+        )
+
+    def add_argument(
+        self,
+        flag: str,
+        default: str,
+        help_: str = "",
+        choices: Sequence[str] | None = None,
+    ) -> None:
+        """
+        Add a command line argument to the parser.
+
+        Args:
+            flag: The flag to use for the argument. (e.g. --flag)
+            default: The default value for the argument.
+            help_: The help text for the argument.
+            choices: The choices for the argument.
+        """
+        self.parser.add_argument(
+            flag,
+            default=default,
+            help=help_,
+            choices=choices,
+        )
+
+    def parse_args(self, args: Sequence[str] | None = None) -> argparse.Namespace:
+        """Parse command line arguments."""
+        self.parser.add_argument(
+            "--logging-level",
+            help="Logging level (default: $LOGGING_LEVEL | INFO)",
+            choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+            default=self.secrets.get("LOGGING_LEVEL", "INFO"),
+        )
+        self.parser.add_argument(
+            "--token",
+            help="PagerDuty API Token (default: $PAGERDUTY_TOKEN)",
+            default=self.secrets.get("PAGERDUTY_TOKEN", ""),
+        )
+        self.parser.add_argument(
+            "--email",
+            help="PagerDuty Email (default: $PAGERDUTY_EMAIL)",
+            default=self.secrets.get("PAGERDUTY_EMAIL", ""),
+        )
+
+        parsed = (
+            self.parser.parse_args() if args is None else self.parser.parse_args(args)
+        )
+
+        self.secrets.set("PAGERDUTY_TOKEN", parsed.token)
+        self.secrets.set("PAGERDUTY_EMAIL", parsed.email)
+        self.secrets.set("LOGGING_LEVEL", parsed.logging_level)
+
+        return parsed
---
+++ tests/util/runtime_init_test.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+import logging
+
+import pytest
+from _pytest.logging import LogCaptureFixture
+
+from pd_util_scripts.util import RuntimeInit
+
+ENV_FILE = "tests/fixture/mockenv"
+
+
+@pytest.fixture
+def runtime() -> RuntimeInit:
+    return RuntimeInit()
+
+
+def test_init_secrets(runtime: RuntimeInit) -> None:
+    runtime.init_secrets(ENV_FILE)
+
+    assert runtime.secrets.get("PAGERDUTY_TOKEN") == "token"
+    assert runtime.secrets.get("PAGERDUTY_EMAIL") == "email"
+    assert runtime.secrets.get("LOGGING_LEVEL") == "CRITICAL"
+
+
+def test_init_logging(runtime: RuntimeInit, caplog: LogCaptureFixture) -> None:
+    prior_level = logging.getLogger().level
+    logger = logging.getLogger("init_logging")
+    try:
+        runtime.secrets.set("LOGGING_LEVEL", "CRITICAL")
+        runtime.init_logging()
+
+        logger.error("error")
+        logger.critical("critical")
+
+    finally:
+        # Restore logging level at root to avoid pollution
+        logging.getLogger().setLevel(prior_level)
+
+    assert "error" not in caplog.text
+    assert "critical" in caplog.text
+
+
+def test_empty_parse_arg_results(runtime: RuntimeInit) -> None:
+    args = runtime.parse_args([])
+
+    assert args.token == ""
+    assert args.email == ""
+    assert args.logging_level == "INFO"
+
+
+def test_environ_parse_arg_defaults(runtime: RuntimeInit) -> None:
+    runtime.secrets.set("PAGERDUTY_TOKEN", "TOKEN")
+    runtime.secrets.set("PAGERDUTY_EMAIL", "EMAIL")
+    runtime.secrets.set("LOGGING_LEVEL", "LOGGING_LEVEL")
+
+    args = runtime.parse_args([])
+
+    assert args.token == "TOKEN"
+    assert args.email == "EMAIL"
+    assert args.logging_level == "LOGGING_LEVEL"
+
+
+def test_parse_arg_cli(runtime: RuntimeInit) -> None:
+    cli = ["--token", "MOCKTOKEN", "--email", "MOCKEMAIL"]
+
+    args = runtime.parse_args(cli)
+
+    assert args.token == "MOCKTOKEN"
+    assert args.email == "MOCKEMAIL"
+
+
+def test_add_arg(runtime: RuntimeInit) -> None:
+    runtime.add_argument("--TEST", "empty", "Goodbye")
+
+    args = runtime.parse_args(["--TEST", "Hello"])
+
+    assert args.TEST == "Hello"
