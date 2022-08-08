52c25ad40bad98ac2ffd14b4924346ed93ec5c7c
Preocts <preocts@gmail.com>
2022-08-07 22:32:23 -0400

    Model for user team - not a Base model

--- pd_utils/model/__init__.py
+++ pd_utils/model/__init__.py
@@ -4,10 +4,12 @@ from .escalation_rule_coverage import EscalationRuleCoverage
 from .incident import Incident
 from .schedule_coverage import ScheduleCoverage
 from .user_report_row import UserReportRow
+from .user_team import UserTeam
 
 __all__ = [
     "EscalationRuleCoverage",
     "Incident",
     "ScheduleCoverage",
     "UserReportRow",
+    "UserTeam",
 ]
