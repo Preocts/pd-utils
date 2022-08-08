b571c397f0c9956c339fa1f996431c4bd123244e
Preocts <preocts@gmail.com>
2022-08-07 22:33:12 -0400

    Hydrate team membership roles from teams endpoint

--- pd_utils/user_report.py
+++ pd_utils/user_report.py
@@ -4,12 +4,20 @@ Pull a user report including general information on user accounts.
 from __future__ import annotations
 
 import logging
+from typing import Any
+from typing import NamedTuple
 
 from pd_utils.model import UserReportRow
+from pd_utils.model import UserTeam
 from pd_utils.util import DateTool
 from pd_utils.util import IOUtil
 from pd_utils.util import PagerDutyQuery
 from pd_utils.util import RuntimeInit
+
+
+class _Team(NamedTuple):
+    team_name: str
+    team_id: str
 
 
 class UserReport:
@@ -37,17 +45,63 @@ class UserReport:
         self._query.set_query_target("/users", "users")
         self._query.set_query_params(
             {
-                "include[]": ["notification_rules", "teams", "contact_methods"],
+                "include[]": ["notification_rules", "contact_methods"],
                 "team_ids[]": team_ids or None,
             }
         )
-        users: list[UserReportRow] = []
+        self.log.info("Pulling user object, this can take a momement.")
+        user_map: dict[str, UserReportRow] = {}
+        teams: set[_Team] = set()
         for resps in self._query.run_iter(limit=self._max_query_limit):
-            self.log.debug("Pulling %d users...", self._max_query_limit)
-            users.extend([UserReportRow.build_from(resp) for resp in resps])
-        self.log.info("Discovered %d users in total.", len(users))
+            for resp in resps:
+                user = UserReportRow.build_from(resp)
+                user_map[user.id] = user
+                teams = teams.union(self._extract_teams(resp["teams"]))
 
-        return IOUtil.to_csv_string(users)
+        self.log.info("Discovered %d users and %d teams.", len(user_map), len(teams))
+
+        user_teams = self._get_team_memberships(teams)
+
+        self._hydrate_team_membership(user_map, user_teams)
+
+        return IOUtil.to_csv_string(list(user_map.values()))
+
+    def _extract_teams(self, teams: list[dict[str, Any]] | None) -> set[_Team]:
+        """Extract unique team_ids from list of teams"""
+        return {_Team(team["summary"], team["id"]) for team in teams or []}
+
+    def _get_team_memberships(self, teams: set[_Team]) -> list[UserTeam]:
+        """Get membership details of teams from PagerDuty."""
+        # TODO: REFACTOR
+
+        self.log.info("Pulling membership details of %d teams.", len(teams))
+        self._query.set_query_params({})
+        user_teams: list[UserTeam] = []
+        for team_name, team_id in teams:
+            self._query.set_query_target(f"/teams/{team_id}/members", "members")
+            for resp in self._query.run_iter(limit=self._max_query_limit):
+                for member in resp:
+                    user_teams.append(
+                        UserTeam(
+                            user_id=member["user"]["id"],
+                            team_id=team_id,
+                            team_name=team_name,
+                            team_role=member["role"],
+                        )
+                    )
+        self.log.info("Discovered %d membership details.", len(user_teams))
+        return user_teams
+
+    def _hydrate_team_membership(
+        self,
+        user_map: dict[str, UserReportRow],
+        user_teams: list[UserTeam],
+    ) -> None:
+        """Hydrate team membership by role into user_map."""
+        for user_team in user_teams:
+            team = f"{user_team.team_name}, {user_team.team_id}"
+            attr = f"{user_team.team_role}_in"
+            getattr(user_map[user_team.user_id], attr).append(team)
 
 
 def main(_args: list[str] | None = None) -> int:
