6baf1c6bba71daf56e6a8a38a367fa5f16c0e400
ionite34 <dev@ionite.io>
2022-10-04 02:47:43 -0400

    More updates for typing to collections.abc

--- pd_utils/util/io_util.py
+++ pd_utils/util/io_util.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import csv
 from io import StringIO
 from typing import Any
-from typing import Sequence
+from collections.abc import Sequence
 
 from pd_utils.model.base import Base
 
